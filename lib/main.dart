1bce0e53ec5d468cc6f88208fd7621f33de3f625
Prashant-Sgh <prashantsingh190804@gmail.com>
2026-03-03 21:08:48 +0530

    initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:aida/features/splash/presentation/widgets/AIDA_animation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: AIDAAnimation(),
         ),
       ),
     );
