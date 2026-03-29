d0e6c4dcf750fc0303f81a100bce1e87167a2335
Prashant-Sgh <prashantsingh190804@gmail.com>
2026-03-30 00:13:14 +0530

    Integrated the custome node backend for chats

--- lib/features/chat/data/repository/messageRepository.dart
+++ lib/features/chat/data/repository/messageRepository.dart
@@ -2,13 +2,13 @@ import 'dart:convert';
 import 'package:aida/features/chat/data/model/message.dart';
 import 'package:aida/features/chat/data/repository/LocalDbManager.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 enum ResponseState { notInitiated, loading, success, error }
 
 class MessageRepository {
-  // final String _baseUrl = 'http://localhost:3000';
-  final String _baseUrl = 'http://192.168.137.1:3000';
+  final String _baseUrl =
+      'https://aida-backend-ee5qdr7fo-prashant-knows-projects.vercel.app';
+  // final String _baseUrl = 'http://192.168.137.1:3000';
 
   ResponseState _responseState = ResponseState.notInitiated;
 
@@ -21,29 +21,24 @@ class MessageRepository {
   Future<String?> sendMessage(String message) async {
     final url = Uri.parse('$_baseUrl/send-message');
     final body = jsonEncode({'latestUserMessage': message});
-    // final headers = jsonEncode({'Content-Type': 'application/json'});
-    final headers = jsonEncode({'Accept': '*/*'});
+    final headers = {
+      'Content-Type': 'application/json',
+      'x-vercel-protection-bypass': 'EVdAY3uz4Y2FsMNKsNMVLudVBt9yXzPh'
+    };
 
     updateResponseState(ResponseState.loading);
     try {
-      print("Message is: request sent...");
-      final response =
-          await http.post(url, headers: {'Accept': '*/*'}, body: body);
-      print("Message is: response received...");
+      final response = await http.post(url, headers: headers, body: body);
       if (response.statusCode == 200) {
         updateResponseState(ResponseState.success);
-        print("Message is: status code 200");
-        print("Message is: status code 200: ${response.body}");
-        return jsonDecode(response.body);
+        return response.body;
       } else {
         updateResponseState(ResponseState.error);
-        print("Message is: status code BAD");
-        return "WTF*";
+        return "error: ${response.statusCode}";
       }
     } catch (error) {
       updateResponseState(ResponseState.error);
-      print("Message is: status code ERROR");
-      return "WTF*";
+      return "error: $error";
     }
   }
 
